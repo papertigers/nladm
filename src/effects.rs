4e79aec7b0ce51b051c72d3e223fc188504b0d65
Mike Zeller <mike@mikezeller.net>
2019-05-07 11:48:47 -0400

    Make setting the brightness at the same time as an effect possible

--- src/effects.rs
+++ src/effects.rs
@@ -1,3 +1,4 @@
+use futures::future::Future;
 use nanoleaf::client::Client;
 use nanoleaf::error::Error;
 use structopt::StructOpt;
@@ -12,14 +13,20 @@ pub struct Effects {
 #[derive(StructOpt, Debug)]
 pub enum EffectsType {
     #[structopt(name = "get")]
-    /// get the current effect
+    /// Get the current effect
     Get,
     #[structopt(name = "list")]
     /// List all effects
     List,
     #[structopt(name = "set")]
     /// Set an effect
-    Set { effect: String },
+    Set {
+        /// Name of the Effect
+        effect: String,
+        /// Brightness level
+        #[structopt(name = "brightness", long = "brightness", short = "b")]
+        brightness: Option<u32>,
+    },
 }
 
 pub fn handle_effect(
@@ -33,6 +40,13 @@ pub fn handle_effect(
         EffectsType::List => rt
             .block_on(c.get_all_effects(t))
             .map(|v| v.iter().for_each(|e| println!("{}", e))),
-        EffectsType::Set { effect } => rt.block_on(c.set_effect(t, &effect)),
+        EffectsType::Set { effect, brightness } => {
+            let effect = c.set_effect(t, &effect);
+            if let Some(b) = brightness {
+                let bri = nanoleaf::Brightness::Set { value: b };
+                return rt.block_on(effect.join(c.set_brightness(t, bri)).map(|_| ()));
+            }
+            rt.block_on(effect)
+        }
     }
 }
